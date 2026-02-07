924174b78dcf3655c6596bd3384efbb60f21fb77
nirmmalesh <darkestday0809@gmail.com>
2026-02-08 00:13:40 +0800

    Add files via upload

--- pie_length_calculator.py
+++ pie_length_calculator.py
@@ -3,27 +3,29 @@
 
 from math import pi, e
 
-calc_running = True
-
 def calculator():
     
+    global calc_running
+    calc_running = True
+
     while calc_running:
         print("this is the pi/e calculator!")
         pie = input("choose between pi and e!: ")
 
-        if pie not in ("pi", "Pi", "PI", "e", "E"):
-            print(f"{pie} is not a valid option!")
-            pie = input("choose between pi and e!: ")
-        else:
-            continue
-
-        accuracy = input("select your degree of accuracy (max 15): ")
-        try: 
-            accuracy = int(accuracy)
-        except ValueError:
-            print(f"{accuracy} is not valid")
-            print("your accuracy must be from 0-15 s.f.")
-            continue
+        while True:
+            if pie not in ("pi", "Pi", "PI", "e", "E"):
+                print(f"{pie} is not a valid option!")
+            else:
+                continue
+        
+        while True:
+            accuracy = input("select your degree of accuracy (max 15): ")
+            try: 
+                accuracy = int(accuracy)
+            except ValueError:
+                print(f"{accuracy} is not valid")
+                print("your accuracy must be from 0-15 s.f.")
+                continue
 
         # comparison statements can't be used in except cases
         # an if statement is used instead
