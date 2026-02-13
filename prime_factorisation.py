03abce5870fdab16314a5f5f795322dc38547c3e
nirmmalesh <darkestday0809@gmail.com>
2026-02-13 10:53:21 +0000

    debugs

--- prime_factorisation.py
+++ prime_factorisation.py
@@ -4,39 +4,41 @@
 from sympy import isprime
 from math import sqrt
 
-def user_inputs():
+global is_running
+is_running = True
 
-    global is_running
-    is_running = True
+def user_inputs():
     
     print("prime factorisation")
     print("this program accepts a natural number from the user and returns its prime factors")
     begin = input("begin? type any key. (q to quit): ")
 
-    while is_running:
-        if begin in ("q", "quit", "Q", "QUIT", "Quit"):
-            break
+    if begin in ("q", "quit", "Q", "QUIT", "Quit"):
+        exit()
 
 def check_input():
-    
-    global number 
 
+    global number 
     while True:
 
-        number = input("what number would you like to factorise?: ")
-
+        number = input("what number would you like to factorise? (q to quit): ")
+        
+        if number in ("q", "quit", "Q", "QUIT"):
+            exit()
+        
         try:
+
             number = int(number)
             if number < 1:
                 print(f"{number} is not valid.")
                 print()
             else:
                 break
+
         except (ValueError, TypeError):
             print(f"{number} is not valid.")
             print("your input must be a natural number")
             print()
-            number = input("what number would you like to factorise? (q to quit): ")
 
 def factorise():
 
@@ -45,16 +47,12 @@ def factorise():
     for num in range (1, number): 
         if number % num == 0 and isprime(num):
             factors_list.append(num)
-            continue
-        else:
-            return factors_list 
-            break
-
+        # no need for continue, doesn't do anything
     print(f"your number, {number}, has these prime factors: {factors_list}")
 
 def main():
 
-     while True:
+    while is_running:
         
         user_inputs()
         check_input()
