d6c0909a477d9573b5e115957b4933659cbe873a
nirmmalesh <darkestday0809@gmail.com>
2026-02-14 12:53:37 +0000

    y

---
+++ quit_option.py
@@ -0,0 +1,12 @@
+def quit_option():
+    ask = input("do you want to restart? (y/n):")
+    if ask in ["n", "N"]:
+        print("thanks for trying!")
+        exit()
+    elif ask in ["y", "Y"]:
+        print("restarting...")
+    else:
+        print(f"{ask} was not valid.")
+        ask = input("do you want to restart? (y/n):")
+
+quit_option()
